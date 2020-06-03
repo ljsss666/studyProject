81c6601da5ca32f9a63904509f68249838f9ca73
ljsss666 <761233645@qq.com>
2020-06-03 22:13:56 +0800

    添加项目文件。

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApp1
+{
+    public class Class1
+    {
+        public String name { get; set; }
+        public int age { get; set; }
+
+      
+    }
+}
---
+++ WpfApp1/MyClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApp1
+{
+   
+
+    public class MyClass
+    {
+
+        private List<Class1> persons { get; set; }
+        public MyClass() {
+            persons = new List<Class1>();
+            persons.Add(new Class1 {name="sss",age=18 });
+            persons.Add(new Class1 { name = "aaa", age = 19 });
+
+        }
+
+   
+    }
+
+   
+}
